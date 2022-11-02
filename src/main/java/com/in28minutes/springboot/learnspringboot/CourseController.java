3468627962e1b077c84fb5b21db631cea9b7a5a1
onidino <lucianodefilippe@gmail.com>
2022-11-02 20:42:30 -0300

    First Push

---
+++ src/main/java/com/in28minutes/springboot/learnspringboot/CourseController.java
@@ -0,0 +1,13 @@
+package com.in28minutes.springboot.learnspringboot;
+
+// courses
+//[
+//    {
+//    "id":1,
+//    "name":"Learn AWS",
+//    "author":"in28minutes"
+//    }
+//    ]
+public class CourseController {
+
+}
