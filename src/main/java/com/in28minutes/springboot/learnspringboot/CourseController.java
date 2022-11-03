e8bad7f1f529ef33f68ffad29b215c66ecf2fee5
onidino <lucianodefilippe@gmail.com>
2022-11-03 00:42:44 -0300

    class + request endpoint controller

--- src/main/java/com/in28minutes/springboot/learnspringboot/CourseController.java
+++ src/main/java/com/in28minutes/springboot/learnspringboot/CourseController.java
@@ -1,13 +1,19 @@
 package com.in28minutes.springboot.learnspringboot;
 
-// courses
-//[
-//    {
-//    "id":1,
-//    "name":"Learn AWS",
-//    "author":"in28minutes"
-//    }
-//    ]
+import java.util.Arrays;
+import java.util.List;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class CourseController {
+
+  @RequestMapping("/courses")
+  public List<Course> retrieveAllCourses() {
+    return Arrays.asList(
+        new Course(1L, "Learn AWS", "in28minutes"),
+        new Course(2L, "Learn DevOps", "in28minutes"),
+        new Course(3L, "Learn Test", "in28minutes"));
+  }
 
 }
